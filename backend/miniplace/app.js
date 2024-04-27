653f2d07600e3e43571fa8970bf8a7f4efdf97e6
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-27 15:35:23 -0500

    Refactoring

--- backend/miniplace/app.js
+++ backend/miniplace/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const { socketServer } = require("./socket");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.use(
   cors({
     origin: "http://localhost:3000",
