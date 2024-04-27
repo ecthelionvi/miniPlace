71f0d7eff95cedc939e111a776b44e770095ffeb
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-27 16:38:27 -0500

    Refactoring

--- run_prod.py
+++ run_prod.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+import signal
+import sys
 
 # Path to the backend directory
 backend_dir = os.path.join("backend", "miniplace")
@@ -18,6 +20,32 @@ serve_command = ["serve", "-s", "build"]
 
 # Set the NODE_ENV environment variable to "production"
 os.environ["NODE_ENV"] = "production"
+
+
+def clear_screen():
+    print("\033[2J\033[H", end="")
+
+
+# Function to handle the keyboard interrupt (Ctrl+C)
+def handle_keyboard_interrupt(signal, frame):
+    stop_processes()
+    clear_screen()
+    sys.exit(0)
+
+
+# Function to stop the PM2 and serve processes
+def stop_processes():
+    # Stop the backend server using PM2
+    subprocess.run(["pm2", "stop", "app"], check=True)
+
+    # Stop the frontend server (serve) by terminating the process
+    if frontend_process.poll() is None:
+        frontend_process.terminate()
+        frontend_process.wait()
+
+
+# Register the keyboard interrupt handler
+signal.signal(signal.SIGINT, handle_keyboard_interrupt)
 
 # Change to the backend directory and start the server using PM2
 os.chdir(backend_dir)
