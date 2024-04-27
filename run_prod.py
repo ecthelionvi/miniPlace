f38dbc1b4a526bdf2414b5a70aafedde0631b567
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-27 17:00:31 -0500

    Refactoring

--- run_prod.py
+++ run_prod.py
@@ -1,64 +1,50 @@
 import subprocess
 import os
 import signal
+import psutil
 import sys
 
-# Path to the backend directory
-backend_dir = os.path.join("backend", "miniplace")
-
-# Path to the frontend directory
-frontend_dir = os.path.join("frontend", "miniplace")
-
-# Command to start the backend server using PM2
-backend_command = ["pm2", "start", "app.js"]
-
-# Command to build the frontend
-build_command = ["npm", "run", "build"]
-
-# Command to serve the frontend using serve package
-serve_command = ["serve", "-s", "build"]
-
-# Set the NODE_ENV environment variable to "production"
 os.environ["NODE_ENV"] = "production"
+
+build_command = ["npm", "run", "build"]
+serve_command = ["serve", "-s", "build"]
+backend_command = ["pm2", "start", "app.js"]
+backend_dir = os.path.join("backend", "miniplace")
+frontend_dir = os.path.join("frontend", "miniplace")
 
 
 def clear_screen():
     print("\033[2J\033[H", end="")
 
 
-# Function to handle the keyboard interrupt (Ctrl+C)
 def handle_keyboard_interrupt(signal, frame):
     stop_processes()
     clear_screen()
     sys.exit(0)
 
 
-# Function to stop the PM2 and serve processes
 def stop_processes():
-    # Stop the backend server using PM2
-    subprocess.run(["pm2", "stop", "app"], check=True)
+    for proc in psutil.process_iter(["pid", "name"]):
+        if proc.info["name"] == "serve":
+            proc.send_signal(signal.SIGINT)
+            proc.wait()
 
-    # Stop the frontend server (serve) by terminating the process
-    if frontend_process.poll() is None:
-        frontend_process.terminate()
-        frontend_process.wait()
+    # Stop the backend server using PM2 with force flag
+    subprocess.run(["pm2", "stop", "app", "--force"], check=True)
+
+    # Kill the PM2 daemon
+    subprocess.run(["pm2", "kill"], check=True)
 
 
-# Register the keyboard interrupt handler
 signal.signal(signal.SIGINT, handle_keyboard_interrupt)
 
-# Change to the backend directory and start the server using PM2
 os.chdir(backend_dir)
 subprocess.run(backend_command, check=True)
 
-# Change to the frontend directory
 os.chdir(os.path.join("..", "..", frontend_dir))
 
-# Build the React app
 subprocess.run(build_command, check=True)
 
-# Serve the built React app using the serve package
-frontend_process = subprocess.Popen(serve_command)
+subprocess.Popen(serve_command)
 
-# Wait for the frontend process to finish
-frontend_process.wait()
+signal.pause()
